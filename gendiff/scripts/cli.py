da49ed61a55c759e85339f8bf26a6ce6e3835188
Sergey Garanin <wwwser11@yandex.ru>
2021-08-04 21:31:10 +0300

    step2

--- gendiff/scripts/cli.py
+++ gendiff/scripts/cli.py
@@ -3,9 +3,12 @@ import argparse
 
 
 def run():
-    parser = argparse.ArgumentParser(prog='gendiff', description='Generate diff')
+    parser = argparse.ArgumentParser(prog='gendiff',
+description='Generate diff')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+                    help='set format of output')
 
     parser.print_help()
 
